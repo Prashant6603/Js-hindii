f349a61319f0b50536fe96c99ba7987b7d4d8142
Prashant6603 <prashantrajpal46@gmail.com>
2025-03-28 13:50:58 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; 
+// alert(3+3) we are using node js not browser
+console.log(3+3)
+
+let name="Prashant"
+let age=13
+let isLoggedin=false
+
+// number => 2 to power 53
+// bigint
+// string=>""
+// boolean=>true/false
+// null=>standalone type
+//  undefined=>
+// symbol => unique
+// object
+
+console.log(typeof null);
