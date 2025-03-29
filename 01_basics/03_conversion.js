beb8790305d4d47f3ad7c016c2a8f4c972e5132d
Prashant6603 <prashantrajpal46@gmail.com>
2025-03-29 05:25:52 +0000

    conversionsss

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -18,5 +18,42 @@
 
 let snumber=33
 let stringnumber=String(snumber)
-console.log(typeof (stringnumber))
-console.log(stringnumber)
+// console.log(typeof (stringnumber))
+// console.log(stringnumber)
+
+// ***************** Operation *********************
+
+let value=11
+let negvalue=-value
+// console.log(negvalue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+
+// let str1="hello"
+// let str2="Prashant"
+// let str3=str1+str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + "2")
+// console.log(1+2)
+// console.log(1+2+"2")
+
+// console.log(+true)
+
+// let num1,num2,num3
+// num1=num2=num3=2+2
+// console.log(num1)
+
+let gamecounter = 100
+
+++gamecounter
+console.log(gamecounter)
+
