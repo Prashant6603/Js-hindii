1a1ca96725dcf1e93b27327278d25bef76d0c0e1
Prashant6603 <prashantrajpal46@gmail.com>
2025-03-29 11:13:09 +0000

    primitive and non-primitive

---
+++ 01_basics/05datatypessss.js
@@ -0,0 +1,27 @@
+// primitive
+// string,null,undefined ,bigint,symbol,number,boolean
+
+
+//Non pimitive datatype
+// Array,object ,function
+
+
+// **************** STack and Heap Meomry *****************
+
+// let myyt="code with me"
+// let mypastyt=myyt
+// mypastyt="code is more"
+// console.log(myyt)
+// console.log(mypastyt)
+
+let bbl1={
+    username : "prashnat rajpal",
+    upi: "prash@gmail",
+
+}
+let bbl2=bbl1
+
+bbl2.upi="dhrub@gmail"
+
+console.log(bbl1.upi)
+console.log(bbl2.upi)
