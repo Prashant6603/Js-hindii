49065ffa197eb39560b4b213edb62663978cd0b3
Prashant6603 <prashantrajpal46@gmail.com>
2025-03-29 03:34:00 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,22 @@
+// let score = "33abc"
+// console.log(typeof (score))
+// let value=Number(score)
+// console.log(typeof (value))
+// console.log(value)
+
+// "33"=>33
+// "33abc"=>NaN
+// true=>1 false=>0
+
+// let isloggedIn=1
+// let booleanisLoggedin=Boolean(isloggedIn)
+// console.log(booleanisLoggedin)
+
+// 1=>True; 0=>False
+// ""=>False
+// "hitesh"=>true
+
+let snumber=33
+let stringnumber=String(snumber)
+console.log(typeof (stringnumber))
+console.log(stringnumber)
