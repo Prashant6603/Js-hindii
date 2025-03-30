741fd24ac99b15c66b4b5deb040d5f0b5fa33e4a
Prashant6603 <prashantrajpal46@gmail.com>
2025-03-30 14:44:14 +0000

    Array in js

---
+++ 02_basics/01array.js
@@ -0,0 +1,51 @@
+// array
+const myarr=[0,1,2,3,4,5]
+// const myhero=["Prashant","Krishna","Tanveer"]
+// console.log(myarr[0])
+// console.log(myhero[1])
+
+// myarr.push(7)
+// console.log(myarr)
+// myarr.pop()
+// console.log(myarr)
+
+//  shift
+// unshift
+// inculde
+// console.log(myarr.includes(9))
+//  index
+
+// convert array to string
+// const newarr = myarr.join()
+// console.log(myarr)
+// console.log(newarr)
+
+// slice => slice the array and store into another array do not change the original array
+
+// splice=> direct change the original array and remaing into anther array
+
+
+
+// ********************** Array Part 2 ***********************
+
+// push -> vohi array main oush karta hai
+// concat => naya array bhejta hai
+
+// const all_new_hero =[...marval_her,...dc_hero]
+// console.log(all_new_hero)
+
+
+
+// flat => agar array ke ander array hoto to 
+// const nsd=[1,2,3,4,[5,6,9,4],5,9,[1,2,[5,8,9,0]]]
+// const nds=nsd.flat(Infinity)
+// console.log(nds)
+
+// webscrap ke liye
+// console.log(Array.isArray("Prashnat"))
+// console.log(Array.from("Prashnat"))
+
+
+// intresting case
+console.log(Array.from({name :"Prashnat"}))
+// it give empty arrya beacuse we can determine the which is input is in array form
