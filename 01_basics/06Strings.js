c9a9f41f7346f686fefbf20bc31d340feb24e5a8
Prashant6603 <prashantrajpal46@gmail.com>
2025-03-30 05:23:27 +0000

    stringssss

---
+++ 01_basics/06Strings.js
@@ -0,0 +1,40 @@
+// old way
+// const name="prashnat"
+// const sname="rajpal"
+// console.log(name + sname +  " good human")
+
+// New Version
+
+// console.log(`Hello my name is ${name} and surname is ${sname}`)
+
+// 2nd type to decalre string 
+// const naame=new String("Prashnat")
+// console.log(naame)
+
+// console.log(naame[0])
+// console.log(naame.length)
+// console.log(naame.toUpperCase())
+// console.log(naame.charAt(3))
+// console.log(naame.indexOf('h'))
+// const newname = naame.substring(0,4)
+// console.log(newname)
+
+// const oldname = naame.slice(-6)
+// console.log(oldname)
+
+// const mama="   Prashnat   "
+// console.log(mama)
+// console.log(mama.trim())
+
+
+const nani = new String("Prashnat-is-a-good-human-being")
+
+console.log(nani.split('-'))
+
+// ********************** Number and Maths **************************
+
+//  number take look at motes
+
+// math
+
+
