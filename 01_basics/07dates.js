84f2c5a8e5c694e8cc81440ad70644ca4d9c4473
Prashant6603 <prashantrajpal46@gmail.com>
2025-03-30 11:51:38 +0000

    dates in js

---
+++ 01_basics/07dates.js
@@ -0,0 +1,10 @@
+// let mydate = new Date()
+// console.log(mydate.toString())
+// console.log(mydate.toDateString())
+// console.log(mydate.toLocaleString())
+
+let timmme="prashnat"
+console.log(timmme.length)
+
+let tim = new String("prashnat")
+console.log(tim.length)
