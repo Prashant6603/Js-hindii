22aeda1a5cdbe3582ddab91a2d2944bbc78d5fb5
Prashant6603 <prashantrajpal46@gmail.com>
2025-04-01 06:47:09 +0000

    control statement

---
+++ 03_controlflow/0ne.js
@@ -0,0 +1,17 @@
+// if
+// if(){
+
+// }
+
+// const score = 100
+// if (score>10){
+//     const power = "fly"
+//     console.log(`User power: ${power}`)
+// } else {
+//     console.log("Nothing is good")
+// }
+
+// short hand noatation
+
+const balance = 1000
+if (balance>500) console.log("test");
