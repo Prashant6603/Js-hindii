c3b5dd7597f701381ba5d4bd2d63c5ec2756d1bf
Prashant6603 <prashantrajpal46@gmail.com>
2025-04-01 20:19:05 +0000

    complete for loop

---
+++ 03_basics/02forloop.js
@@ -0,0 +1,94 @@
+//  for loop
+
+// for (let i= 0; i< 10;i++){
+//     const element =i
+//     console.log(element)
+    
+// }
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+
+
+
+// +++++*****+++++*****     While Loop      *****+++++*****+++++
+
+// let arr=0
+// while (arr<3) {
+//     console.log(`The number is ${arr}`)
+//     arr +=1
+// }
+
+
+
+
+
+
+// ++++++++++++++ do while +++++++++++++
+
+
+// let arr = 0
+// do {
+//     console.log(`the number is ${arr}`)
+//     arr +=1
+// } while (arr<5);
+
+
+//  *********   Higher Order For Loop ************
+
+// for of loop
+
+// const arr =[1,2,3,4,5]
+// for (const element of arr) {
+//     console.log(element)
+// }
+
+
+
+//  Map
+
+const map = new Map()
+map.set('In',"India")
+map.set('USA',"Unites states of america")
+map.set('Fr',"France")
+
+// console.log(map)
+
+// for (const element of map) {
+//     console.log(element)
+    
+// }
+
+
+// for (const [key, value] of map) {
+//     console.log(key, ':-', value);
+    
+// }
+
+//     Maps are iterable
+//   objects are not 
+
+
+
+
+// For Each 
+
+// const arr=["js","css","html","py","ipynb"]
+// arr.forEach( function (item){
+
+// })
+
+//  arrow function bhi declare kar sakte hai
+
+
+
+// filter use kare agar for each na kam kare
+
+
+// map
+
+
+// reduce
