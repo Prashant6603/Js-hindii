0b42be166f83c79545ee1cbf4db24b0e53be561c
Prashant6603 <prashantrajpal46@gmail.com>
2025-04-01 07:46:39 +0000

    control

---
+++ 03_basics/01controlflow.js
@@ -0,0 +1,87 @@
+// if
+// if(){
+
+// }
+
+// const score = 100
+// if (score>10){
+//     const power = "fly"
+//     console.log(`User power: ${power}`)
+// } else {
+//     console.log("Nothing is good")
+// }
+
+// short hand noatation
+
+// const balance = 1000
+// if (balance>500) console.log("test");
+
+// multiple if else
+
+// multiple cases and take some variable give them true and use && and || in if else
+
+// switch case
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3
+// switch (month) {
+//     case 1:
+//         console.log("January")
+//         break;
+//     case 2:
+//         console.log("January")
+//         break;
+//     case 3:
+//         console.log("January")
+//         break;
+//     case 4:
+//         console.log("January")
+//         break;
+//     case 5:
+//         console.log("January")
+//         break;
+
+//     default:
+//         break;
+// }
+
+// Falsy values
+
+// false ,0,-0, BigInt 0n, "" ,null , undefined,NaN
+
+// truthy value
+
+// "0" ,"false"," ",[],{},fuunction(){}
+
+// check array and object using length function equal to 0 then we vet value
+
+
+// nullish Coalescing Operator (??): null undefined
+
+// let val1;
+// val1 = 5 ?? 10
+// console.log(val1)
+
+// database se direct response nhi aata hai 2 value milti hai 
+
+
+
+
+//  Terniary operator
+
+
+// conditon ? true:false
+
+
+
+
+const iceTeaspricee =100
+iceTeaspricee <= 80 ? console.log("less than 80"): console.log("more than 80")
