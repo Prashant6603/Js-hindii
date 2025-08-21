86991c5accbfc0dbd937621f4b0b1109392f781e
Prashant6603 <prashantrajpal46@gmail.com>
2025-08-21 07:58:32 +0000

    datatypesss

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,11 +1,11 @@
 "use strict"; // treat all JS code as a newer version
 
 // alert(3+3) we are using node js not browser
-console.log(3+3)
+console.log(3 + 3)
 
-let name="Prashant"
-let age=13
-let isLoggedin=false
+let name = "Prashant"
+let age = 13
+let isLoggedin = false
 
 // number => 2 to power 53
 // bigint
@@ -13,7 +13,9 @@ let isLoggedin=false
 // boolean=>true/false
 // null=>standalone type
 //  undefined=>
-// symbol => unique
+// symbol => unique // mostly use in react
+
+
 // object
 
 console.log(typeof null);
